efb96b795e528111be716b256a0a05573e43b492
Евгений Цитцер <evgenc1980@mail.ru>
2024-05-24 09:10:11 +0700

    Add files via upload

---
+++ homeworkMod2_3.py
@@ -0,0 +1,7 @@
+cars = ['SUBARU', 'VW', 'BMW', 'LADA', 'JEEP']
+for i in cars:
+        print('Я езжу на автомобиле марки', i)
+cars_count = 0
+for i in cars:
+        cars_count += 10
+        print(cars_count)
